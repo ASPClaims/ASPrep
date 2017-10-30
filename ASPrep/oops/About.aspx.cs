a29ead7e0238a19ee0ac61e130eb25fe5b8426a9
santosh <santosh@3frameslab.com>
2017-10-31 00:04:24 +0530

    greeting naresh bro

--- ASPrep/oops/About.aspx.cs
+++ ASPrep/oops/About.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +12,7 @@ namespace oops
         protected void Page_Load(object sender, EventArgs e)
         {
             // Hello Git
+// naresh bhai
         }
     }
 }
